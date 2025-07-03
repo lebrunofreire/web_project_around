3838b52a268ddc9bda919eafd3b7c2f876f7ddab
Leobruno <leobrunofreire10@gmail.com>
2025-07-03 17:53:03 -0300

    Ajustar card

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,28 @@
 const initialCards = [
   {
     name: "Vale de Yosemite",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg"
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
   },
   {
     name: "Lago Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg"
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
   },
   {
     name: "Montanhas Carecas",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg"
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
   },
   {
     name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg"
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
   },
   {
     name: "Parque Nacional da Vanoise ",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg"
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
   },
   {
     name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg"
-  }
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+  },
 ];
 
 const openModalBtn = document.getElementById("openModalBtn");
@@ -51,35 +51,35 @@ document.addEventListener("DOMContentLoaded", () => {
     imageUrlInput.value = "";
   });
 
-savePlaceBtn.addEventListener("click", (event) => {
-  event.preventDefault(); // Evita o envio automático
-  const title = titleInput.value;
-  const imageUrl = imageUrlInput.value;
+  savePlaceBtn.addEventListener("click", (event) => {
+    event.preventDefault(); // Evita o envio automático
+    const title = titleInput.value;
+    const imageUrl = imageUrlInput.value;
 
-  if (!title || !imageUrl) {
-    return alert("Por favor, preencha todos os campos.");
-  }
-
+    if (!title || !imageUrl) {
+      return alert("Por favor, preencha todos os campos.");
+    }
 
     const newCard = document.createElement("div");
     newCard.classList.add("element");
 
     newCard.innerHTML = `
-      <div class="element-header">
-        <button class="delete-btn"></button>
-      </div>
-      <img class="element-image" src="${imageUrl}" alt="${title}">
-      <button class="element-delete-btn"></button>
-      <div class="element-footer">
-        <h4 class="element-image-title">${title}</h4>
-        <button class="element-image-like"></button>
-      </div>
+      <div class="element">
+          <img class="element-image" src="${imageUrl}" alt="${title}" />
+          <button class="element-delete-btn"></button>
+          <p class="element-image-title">
+            ${title}
+            <button class="element-image-like"></button>
+          </p>
+        </div>
       `;
 
     // Adiciona evento de remover
-    newCard.querySelector(".delete-btn").addEventListener("click", (e) => {
-      e.target.closest(".element").remove();
-    });
+    newCard
+      .querySelector(".element-delete-btn")
+      .addEventListener("click", (e) => {
+        e.target.closest(".element").remove();
+      });
 
     elementsContainer.prepend(newCard); // ou appendChild para adicionar no final
 
@@ -94,11 +94,9 @@ openModalBtn.addEventListener("click", () => {
   modal.style.display = "flex";
 });
 
-
 closeModalBtn.addEventListener("click", () => {
   modal.style.display = "none";
 });
-
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -112,20 +110,20 @@ form.addEventListener("submit", (e) => {
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll('.element-delete-btn').forEach(btn => {
-      btn.addEventListener('click', (e) => {
-        const card = e.target.closest('.element');
-        if (card) {
-          card.remove();
-        }
-      });
+  document.querySelectorAll(".element-delete-btn").forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const card = e.target.closest(".element");
+      if (card) {
+        card.remove();
+      }
     });
   });
+});
 
-  document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll('.element-image-like').forEach(btn => {
-    btn.addEventListener('click', () => {
-      btn.classList.toggle('liked');
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".element-image-like").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      btn.classList.toggle("liked");
     });
   });
 });
